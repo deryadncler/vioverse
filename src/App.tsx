d87f6cf77a80752005fa1293df63544b123cf897
deryadncler <deryadncler@hotmail.com>
2026-07-31 14:54:06 +0300

    feat: initialize Vioverse project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar/navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>Welcome to Vioverse 💜</h1>
+    </>
+  );
+}
+
+export default App;
